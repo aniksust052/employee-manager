b4eb4fd1b369ace1427b031f33ba64bea07c08e5
aniksust052 <aniksust052@gmail.com>
2024-12-03 00:28:15 +0600

    backend deployed on lobiq server

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 class ApiServices {
-    static deployedUrl = 'https://employee-spring-boot-production.up.railway.app';
+    // static deployedUrl = 'https://employee-spring-boot-production.up.railway.app';
+    static deployedUrl = 'https://lobiq.xyz';
     static localUrl = 'http://localhost:8080';
 
     static async signUP (user) {
